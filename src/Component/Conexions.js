6a013d12fc3be65731be7fec38808df572dc1754
Romain Demotier <69152836+Romain-demotier@users.noreply.github.com>
2020-11-11 17:05:09 +0100

    mise en d'une page connectée et d une page d erreur en fonction de l url via la librairie rooter

--- src/Component/Conexions.js
+++ src/Component/Conexions.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
+import { useHistory } from "react-router-dom";
 
-function Conexions () {
+function Conexions() {
+    
+    const [pseudo, setpseudo] = useState("");
+    const [acces, setacces] = useState(false);
+    
+    
+    const handleChange = (event) =>{
+        const psd = event.target.value
+        setpseudo(psd)
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setacces(true)
+    }    
+    
+    let history = useHistory();            
+    if (acces){ history.push(`/pseudo/${pseudo}`)};
+            
 
-    return(
-        
-    <div className = "connexionBox">
-        <form className = "connexion">
+                    return(
+                        
+                        <div className = "connexionBox">
+        <form onSubmit= {handleSubmit} className = "connexion">
             <input  type = "text"
                     required
-                    placeholder = "Pseudo">
+                    placeholder = "Pseudo"
+                    onChange = {handleChange}
+                    value = {pseudo}
+                    >
             </input>
             <button type = "submit">Soumetre</button>
         </form>
     </div>
     );
 }
+
 export default Conexions;
